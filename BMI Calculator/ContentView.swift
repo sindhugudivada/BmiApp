0ed21144ddcc8c67158bbbf16a94c22e3be02c55
Sindhu Gudivada <sindhugudivada67@gmail.com>
2019-10-21 20:34:55 -0700

    Initial Commit

---
+++ BMI Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BMI Calculator
+//
+//  Created by Sindhu Gudivada on 10/21/19.
+//  Copyright © 2019 abc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
